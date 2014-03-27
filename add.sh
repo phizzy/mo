cf60e4435ac4462b56e1d29b9cae35cbdae1dcc2
PiHiZi <pihizi@msn.com>
2014-03-27 13:53:06 +0800

    prepare

---
+++ add.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]
+then
+    echo "请输入新建文章的名称，如：2014-04-03-文章"
+    exit 1
+fi
+
+if [ -f "_posts/$1.markdown" ]
+then
+    echo "文章$1重名"
+fi
+
+mkdir -p "images/$1/"
+
+touch _posts/"$1".markdown
